1199996db65405f7fa0aa997a1547a45f92be9ff
Graham Rhodes <grhodes@gilt.com>
2016-06-21 18:45:21 +0900

    Preparing release 10.7.7

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ if not os.path.exists('~/.nova/'):
 install_reqs = parse_requirements('requirements.txt', session=False)
 
 setup(name='gilt-nova',
-    version='10.7.7.dev0',
+    version='10.7.7',
     description="Collection of utilities to easily deploy services to AWS.",
     long_description="Collection of utilities to easily deploy services to AWS.",
     license='Apache License 2.0',
