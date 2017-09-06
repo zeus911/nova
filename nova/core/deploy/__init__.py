412fcd7562d128ce3ef12dd74346e40b18e6e575
Barry O'Neill <barra.oneill@gmail.com>
2017-09-06 12:38:23 +0100

    Issue #12 - Make $DOCKER_ARGS optional

--- nova/core/deploy/__init__.py
+++ nova/core/deploy/__init__.py
@@ -21,7 +21,8 @@ REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/documen
 STACK_ENV=`aws ec2 describe-tags --filters "Name=resource-id,Values=$INSTANCE_ID" "Name=key,Values=Environment" --region=$REGION --output=text | awk '{print $5}'`
 
 . /opt/nova/environments/$STACK_ENV/set-env-vars.sh
-[ -z "$DOCKER_ARGS" ] && { echo "DOCKER_ARGS was not defined" >&2 ; exit 1; }
+
+[ -z "$DOCKER_ARGS" ] && { echo "DOCKER_ARGS was unset, ignoring." >&1; }
 [ -z "$DOCKER_OPTS" ] && { echo "DOCKER_OPTS was not defined" >&2 ; exit 1; }
 [ -z "$DOCKER_VARS" ] && { echo "DOCKER_VARS was not defined" >&2 ; exit 1; }
 [ -z "$DOCKER_VOLS" ] && { echo "DOCKER_VOLS was not defined" >&2 ; exit 1; }
